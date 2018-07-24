ffc2cfc7ec09c5cda3ba04442991b64b156db7c6
Erik Goossens <erikmgoossens@gmail.com>
2018-07-24 12:05:06 -0400

    Initial Commit

---
+++ SizeClassPracticce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassPracticce
+//
+//  Created by Erik Goossens on 2018-07-24.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
